ed614d98318a8641c838dbee693dfda4e00ddad1
JosiahHamer <37419256+JosiahHamer@users.noreply.github.com>
2020-09-21 17:35:36 -0600

    blog

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 from django.views.generic import ListView, DetailView
 
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+from django.urls import reverse_lazy
 
 from .models import Post
 
@@ -27,3 +29,19 @@ class BlogCreateView(CreateView):
 	template_name = 'post_new.html'
 
 	fields = ['title', 'author', 'body']
+
+class BlogUpdateView(UpdateView):
+
+	model = Post
+
+	template_name = 'post_edit.html'
+
+	fields = ['title', 'body']
+
+class BlogDeleteView(DeleteView):
+	
+	model = Post
+
+	template_name = 'post_delete.html'
+
+	success_url = reverse_lazy('home')
